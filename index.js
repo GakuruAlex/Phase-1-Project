d5cf43f04f17c2eb93f4b6d24b1ac2eed93d3dc7
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-30 12:01:36 +0300

    Changed to Booking Com API and hid api key

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+document.addEventListener(`DOMContentLoaded`,lauchApp);
+require(`dotenv`).config();
+const apiKey=process.env.API_KEY;
 
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'c25be73193mshec5a19bdd082cc9p128ae6jsnd95fa665178f',
-            'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
-        }
-    };
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key':apiKey,
+		'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+	}
+};
 
-    fetch('https://airbnb19.p.rapidapi.com/api/v1/getLanguages', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err))
+fetch('https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=AED&locale=en-gb', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
