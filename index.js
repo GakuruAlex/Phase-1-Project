5ee990548e0bae25059a880f19dcfa80ed46ffaf
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-09-01 21:34:50 +0300

    Added a collision avoidance function

--- index.js
+++ index.js
@@ -52,3 +52,54 @@ let generateShipLocations=function() {
   this.ships[i].locations = locations;
   }
 }
+
+
+let generateShip=function() {
+  let direction = Math.floor(Math.random() * 2);
+  let row, col;
+
+
+  if (direction === 1) {
+  // Generate a starting location for a horizontal ship
+  row = Math.floor(Math.random() * this.boardSize);
+  col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+  } else {
+
+  // Generate a starting location for a vertical ship
+  row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+
+col = Math.floor(Math.random() * this.boardSize);
+}
+
+let newShipLocations = [];
+for (let i = 0; i < this.shipLength; i++) {
+if (direction === 1) {
+
+newShipLocations.push(row + "" + (col + i));
+
+} else {
+newShipLocations.push((row + i) + "" + col);
+
+
+}
+
+
+}
+return newShipLocations;
+}
+
+
+let collision= function(locations) {
+
+  for (var i = 0; i < this.numShips; i++) {
+
+  let ship = model.ships[i];
+
+  for (var j = 0; j < locations.length; j++) {
+  if (ship.locations.indexOf(locations[j]) >= 0) {
+  return true;
+  }
+  }
+  }
+  return false;
+  }
