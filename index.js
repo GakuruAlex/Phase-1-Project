0d0a13a7b9e20685d1a90320ff7d5c8c38382832
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-09-01 21:54:55 +0300

    Added check hit status function

--- index.js
+++ index.js
@@ -56,6 +56,45 @@ const model = {
   { locations: [0, 0, 0], hits: ["", "", ""] } ],
   }
 
+  let fire= function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+    let ship = this.ships[i];
+    let index = ship.locations.indexOf(guess);
+    if (index >= 0) {
+    ship.hits[index] = "hit";
+
+    view.displayHit(guess);
+
+    view.displayMessage("HIT!");
+    if (this.isSunk(ship)) {
+    view.displayMessage("You sank my battleship!");
+    this.shipsSunk++;
+
+    }
+
+    return true;
+    }
+    }
+    view.displayMiss(guess);
+
+    view.displayMessage("You missed.");
+
+    return false;
+
+    };
+
+    let isSunk=function(ship) {
+
+    for (let i = 0; i < this.shipLength; i++) {
+    if (ship.hits[i] !== "hit") {
+
+    return false;}
+
+
+     else  return true;
+    }}
+
+
 let generateShipLocations=function() {
 
   let locations;
